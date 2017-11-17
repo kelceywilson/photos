aa00678aeefcef2e2ed008240f35199d38dfd2f6
Kelcey Wilson <kelceywilson@gmail.com>
2017-11-16 16:56:36 -0800

    updated vote update route, but it does not work yet

--- routes/routes.js
+++ routes/routes.js
@@ -1,28 +1,29 @@
 const express = require('express');
-const { getVotes } = require('../db/db.js')
-
-// const db = require('../db/client.js');
-const http = require('http');
+const { db } = require('../db/db.js')
 
 const router = express.Router();
 
 // GET home page - main.pug
-// include any existing variables
 router.get('/', (req, res) => {
-  // As a user, I want to see a random image when I go to the '/' route for the site.
   // function to give random number between 0 and 49
   let randomNumber = Math.round(Math.random() * 49)
-  // we need to do AJAX request to pull the random photo
+  // AJAX request to pull the random photo
   res.render('layout', { randomNumber: randomNumber });
 });
 
-router.get('/votes', (req, res) => {
-
-  // console.log('voted');
-  // res.json({
-  //   response: 'error',
-  //   body: error,
-  // });
+// update upVote count -- need to update downVote
+router.put('/votes', (req, res) => {
+  let newCount = 0
+  const votes = db.any('SELECT upVotes FROM votes WHERE photo_id=$1', req.body.id)
+    .then((returnedCount) => {
+      console.log(returnedCount[0].upvotes);
+      newCount = returnedCount[0].upvotes + 1
+      db.oneOrNone('UPDATE photos SET upVotes=$1 WHERE photo_id=$2 RETURNING photo_id', [newCount, req.body.id])
+        .then((returnedNewCount) => {
+          console.log(returnedNewCount);
+        })
+        .catch(err => Object({ success: false, message: err.message }))
+    })
 })
 
 module.exports = router;

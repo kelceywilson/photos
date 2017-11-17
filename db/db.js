07122ada453d5830df1c70335ae5ba1dace35cdf
Kelcey Wilson <kelceywilson@gmail.com>
2017-11-16 16:50:09 -0800

    removed all db functions - now only a db connection

--- db/db.js
+++ db/db.js
@@ -6,28 +6,6 @@ const connection = process.env.NODE_ENV === 'test'
 
 const db = pgp(connection)
 
-const getVotes = (id) => {
-  // get votes of current photo
-  return db.any('SELECT upVotes, downVotes FROM votes WHERE photo_id=$1', id)
-  // return id
-}
-const upVote = () => {
-  // maybe get the current count first, then add 1 then send to update query
-  // db.oneOrNone('UPDATE photos SET count=$1 WHERE photo_id=$2 RETURNING photo_id', [newName, petId])
-  //   .then((returnedId) => {
-  //     if (returnedId) return { success: true, message: 'Name changed!' }
-  //     return { success: false, message: `Could not find petId ${petId}` }
-  //   })
-  //   .catch(err => Object({ success: false, message: err.message }))
-}
-
-const downVote = () => {
-
-}
-
 module.exports = {
-  db,
-  getVotes,
-  upVote,
-  downVote,
+  db
 }

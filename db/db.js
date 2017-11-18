18cd064549efe200fc0313f9cd04991607634ce2
Kelcey Wilson <kelceywilson@gmail.com>
2017-11-17 16:50:31 -0800

    * Got voting function working in routes.js * -- but unable to transfer the function to db.js * -- getting unexpected character in json on client

--- db/db.js
+++ db/db.js
@@ -6,6 +6,29 @@ const connection = process.env.NODE_ENV === 'test'
 
 const db = pgp(connection)
 
+const vote = (id, direction) =>
+  db.oneOrNone('SELECT photo_id, upvotes, downvotes FROM votes WHERE photo_id = $1', id)
+    .then((results) => {
+      if(!results){
+        // insert new record with either 1 upvote or one downvote
+        db.one(`INSERT INTO votes(photo_id, ${direction}) VALUES($1, $2) RETURNING upvotes, downvotes`, [id, 1])
+        .then((newresults1) => {
+          console.log('1', newresults1);
+          return newresults1;
+        });
+      } else {
+        let newCount = results[direction] + 1;
+        db.oneOrNone(`UPDATE votes SET ${direction}=$1 WHERE photo_id=$2 RETURNING upvotes, downvotes`, [newCount, id])
+        .then((newresults2) => {
+          console.log('2', newresults2);
+          return newresults2;
+        })
+        .catch(err => Object({ success: false, message: err.message }))
+      }
+    })
+    .catch(err => Object({ success: false, message: err.message }))
+
 module.exports = {
-  db
+  db,
+  vote,
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,31 +1,41 @@
 const express = require('express');
-const { db } = require('../db/db.js')
+const { db, vote } = require('../db/db.js')
 
 const router = express.Router();
 
 // GET home page - main.pug
 router.get('/', (req, res) => {
-  // function to give random number between 0 and 49
-  let randomNumber = Math.round(Math.random() * 49)
-  // AJAX request to pull the random photo
-  res.render('layout', { randomNumber: randomNumber });
+  res.render('layout');
 });
 
 // update upVote count -- need to update downVote
 router.put('/votes', (req, res) => {
-  let newCount = 0
-  const votes = db.any('SELECT upvotes FROM votes WHERE photo_id=$1', req.body.id)
-    .then((returnedCount) => {
-      console.log(returnedCount[0].upvotes);
-      newCount = returnedCount[0].upvotes + 1
-      db.oneOrNone('UPDATE votes SET upvotes=$1 WHERE photo_id=$2 RETURNING upvotes, downvotes', [newCount, req.body.id])
-        .then((returnedNewCount) => {
-          console.log('newCount', returnedNewCount)
-          // console.log(returnedNewCount.isArray());
-          res.json(returnedNewCount);
-        })
-        .catch(err => Object({ success: false, message: err.message }))
+  console.log(req.body.id);
+  // vote(req.body.id, req.body.direction)
+  //   .then((results) => {
+  //     console.log('here', results);
+  //     res.json(results)
+  //   })
+  //   .catch(console.error)
+
+  db.oneOrNone('SELECT photo_id, upvotes, downvotes FROM votes WHERE photo_id = $1', req.body.id)
+    .then((results) => {
+      if(!results){
+        // insert new record with either 1 upvote or one downvote
+        db.one(`INSERT INTO votes(photo_id, ${req.body.direction}) VALUES($1, $2) RETURNING upvotes, downvotes`, [req.body.id, 1])
+          .then((newresults1) => {
+            res.json(newresults1);
+          });
+      } else {
+        let newCount = results[req.body.direction] + 1;
+        db.oneOrNone(`UPDATE votes SET ${req.body.direction}=$1 WHERE photo_id=$2 RETURNING upvotes, downvotes`, [newCount, req.body.id])
+          .then((newresults2) => {
+            res.json(newresults2);
+          })
+          .catch(err => Object({ success: false, message: err.message }))
+      }
     })
+    .catch(err => Object({ success: false, message: err.message }))
 })
 
 module.exports = router;

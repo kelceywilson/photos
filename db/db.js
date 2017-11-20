284f302c300fe02a515c00dfaf022e8eeee568de
Kelcey Wilson <kelceywilson@gmail.com>
2017-11-20 10:47:31 -0800

    updated vote function to enable cancelling previous vote

--- db/db.js
+++ db/db.js
@@ -6,19 +6,20 @@ const connection = process.env.NODE_ENV === 'test'
 
 const db = pgp(connection)
 
-const vote = (id, direction) =>
+const vote = (id, direction, amount) =>
   db.oneOrNone('SELECT photo_id, upvotes, downvotes FROM votes WHERE photo_id = $1', id)
     .then((results) => {
       if(!results){
         // insert new record with either 1 upvote or one downvote
-        db.one(`INSERT INTO votes(photo_id, ${direction}) VALUES($1, $2) RETURNING upvotes, downvotes`, [id, 1])
+        return db.one(`INSERT INTO votes(photo_id, ${direction}) VALUES($1, $2) RETURNING upvotes, downvotes`, [id, 1])
         .then((newresults1) => {
           console.log('1', newresults1);
           return newresults1;
         });
       } else {
-        let newCount = results[direction] + 1;
-        db.oneOrNone(`UPDATE votes SET ${direction}=$1 WHERE photo_id=$2 RETURNING upvotes, downvotes`, [newCount, id])
+        let newCount = results[direction] + amount;
+        console.log(typeof amount, typeof results[direction]);
+        return db.oneOrNone(`UPDATE votes SET ${direction}=$1 WHERE photo_id=$2 RETURNING upvotes, downvotes`, [newCount, id])
         .then((newresults2) => {
           console.log('2', newresults2);
           return newresults2;

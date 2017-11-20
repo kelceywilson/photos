ba56d3210f7133e8cbdea91b009042ef7b40be8f
Kelcey Wilson <kelceywilson@gmail.com>
2017-11-20 10:49:21 -0800

    update voteSave function to allow cancelling previous vote

--- public/js/main.js
+++ public/js/main.js
@@ -3,13 +3,21 @@ const image = document.getElementById('image');
 const thumbsUp = document.getElementById('up');
 const thumbsDown = document.getElementById('down');
 const electionUpdate = document.getElementById('electionUpdate')
+
+// randomNumber variable to hold photo_id
+let randomNumber
+// save current vote state so that unvote can be made instead of repeating vote
+let direction = 'cleared';
+
 const upVote = () => {
-  thumbsUp.classList.add('fa-thumbs-up')
-  thumbsUp.classList.remove('fa-thumbs-o-up')
+    thumbsUp.classList.add('fa-thumbs-up')
+    thumbsUp.classList.remove('fa-thumbs-o-up')
+    direction = 'upvotes'
 }
 const downVote = () => {
   thumbsDown.classList.add('fa-thumbs-down')
   thumbsDown.classList.remove('fa-thumbs-o-down')
+  direction = 'downvotes'
 }
 const clearDownVote = () => {
   thumbsDown.classList.remove('fa-thumbs-down')
@@ -19,30 +27,20 @@ const clearUpVote = () => {
   thumbsUp.classList.remove('fa-thumbs-up')
   thumbsUp.classList.add('fa-thumbs-o-up')
 }
-// const unvote = () => {
-//   // this will remove a vote for the current picture
-//   // use random number from image.src?
-//   console.log('unvote');
-// }
-//
-let randomNumber
+
 const newImage = () => {
   randomNumber = Math.round(Math.random() * 49)
   image.src = "https://picsum.photos/800/500/?image=" + randomNumber
   return randomNumber
 }
 image.addEventListener('click', function(){
-  // randomNumber = Math.round(Math.random() * 49)
-  // image.src = "https://picsum.photos/800/500/?image=" + randomNumber
   randomNumber = newImage()
   clearUpVote()
   clearDownVote()
   electionUpdate.innerHTML = ''
 })
 
-function voteSave(direction) {
-  console.log('fetching');
-  console.log('data', randomNumber)
+function voteSave(amount) {
   return fetch('/votes', {
     method: 'put',
     headers: {
@@ -51,7 +49,8 @@ function voteSave(direction) {
     },
     body: JSON.stringify({
       id: randomNumber,
-      direction: direction
+      direction: direction,
+      amount: amount
     })
   })
   .then(updatedVoteCount => updatedVoteCount.json())
@@ -59,35 +58,43 @@ function voteSave(direction) {
 
 // addEventListeners to thumbs to alter appearance when clicked
 thumbsUp.addEventListener('click', function(){
-  upVote()
-  clearDownVote()
-  voteSave('upvotes')
+  console.log(direction);
+  if(direction === 'upvotes'){
+    console.log('function to subtract upvote from db');
+    voteSave(-1)
+    clearUpVote()
+    direction = 'cleared'
+  } else {
+    upVote()
+    clearDownVote()
+    voteSave(1)
     .then((election)=>{
       console.log(election);
       electionUpdate.innerHTML = `Upvotes: ${election.upvotes}\nDownvotes: ${election.downvotes}`
     })
+  }
 })
 
 thumbsDown.addEventListener('click', function(){
-  downVote()
-  clearUpVote()
-  voteSave('downvotes')
-    .then((election)=>{
-      console.log(election);
-      electionUpdate.innerHTML = `Upvotes: ${election.upvotes}\nDownvotes: ${election.downvotes}`
-    })
+  console.log(direction);
+  if(direction === 'downvotes'){
+    console.log('function to subtract downvote from db');
+    voteSave(-1)
+    clearDownVote()
+    direction = 'cleared'
+  } else {
+    downVote()
+    clearUpVote()
+    voteSave(1)
+      .then((election)=>{
+        console.log(election);
+        electionUpdate.innerHTML = `Upvotes: ${election.upvotes}\nDownvotes: ${election.downvotes}`
+      })
+  }
 })
 
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
-    // document ready
     randomNumber = newImage()
-
   }
 };
-//
-//  and to display vote count after first clicked
-//    vote count should be stored in db
-//  and to allow users to unclick icon
-// fa fa-thumbs-down
-// fa fa-thumbs-up

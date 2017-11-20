79d989a35bbad5ffe74ff2672e0b81a62e384ae5
Kelcey Wilson <kelceywilson@gmail.com>
2017-11-20 10:50:50 -0800

    added 'amount' variable to function to tell db query whether to add or subtract a vote

--- routes/routes.js
+++ routes/routes.js
@@ -3,39 +3,20 @@ const { db, vote } = require('../db/db.js')
 
 const router = express.Router();
 
-// GET home page - main.pug
+// GET home page - layout.pug
 router.get('/', (req, res) => {
   res.render('layout');
 });
 
-// update upVote count -- need to update downVote
+// PUT votes into database
 router.put('/votes', (req, res) => {
-  console.log(req.body.id);
-  // vote(req.body.id, req.body.direction)
-  //   .then((results) => {
-  //     console.log('here', results);
-  //     res.json(results)
-  //   })
-  //   .catch(console.error)
-
-  db.oneOrNone('SELECT photo_id, upvotes, downvotes FROM votes WHERE photo_id = $1', req.body.id)
+  console.log(req.body.id, req.body.direction, req.body.amount);
+  vote(req.body.id, req.body.direction, req.body.amount)
     .then((results) => {
-      if(!results){
-        // insert new record with either 1 upvote or one downvote
-        db.one(`INSERT INTO votes(photo_id, ${req.body.direction}) VALUES($1, $2) RETURNING upvotes, downvotes`, [req.body.id, 1])
-          .then((newresults1) => {
-            res.json(newresults1);
-          });
-      } else {
-        let newCount = results[req.body.direction] + 1;
-        db.oneOrNone(`UPDATE votes SET ${req.body.direction}=$1 WHERE photo_id=$2 RETURNING upvotes, downvotes`, [newCount, req.body.id])
-          .then((newresults2) => {
-            res.json(newresults2);
-          })
-          .catch(err => Object({ success: false, message: err.message }))
-      }
+      console.log('here', results);
+      res.json(results)
     })
-    .catch(err => Object({ success: false, message: err.message }))
+    .catch(console.error)
 })
 
 module.exports = router;
